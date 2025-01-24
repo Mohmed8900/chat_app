6cb759ec0485d358cff837d23ca2498ad9420739
Mohmed8900 <mohamed.m.1985@icloud.com>
2025-01-24 22:52:11 +0400

    Update App.js

--- Src/App.js
+++ Src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import AdminDashboard from "./pages/AdminDashboard";
-import UserDashboard from "./pages/UserDashboard";
-import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/user" element={<UserDashboard />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a test page.</p>
+    </div>
   );
 }
 
